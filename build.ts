6d6b25e2ef3220f4ce8546e4174dbde9d9a34b00
David Daza <03dazal@gmail.com>
2025-12-10 10:05:15 -0500

    index on build

--- build.ts
+++ build.ts
@@ -1,3 +1,8 @@
+import { indexTools } from "./ingest"
+import { toolDefinitions } from "./tools/definitions"
+
+await indexTools(toolDefinitions)
+
 await Bun.build({
     entrypoints: ["./index.ts"],
     outdir: "./build",
